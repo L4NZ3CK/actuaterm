88695f752ae9b5cacd9949643d366a732d58053c
L4NZ3CK <111375475+L4NZ3CK@users.noreply.github.com>
2022-08-17 07:25:48 -1200

    Add files via upload

---
+++ actuaterm/actuaterm.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+echo "Created by: L4N3CK"
+echo "Github: https://github.com/L4NZ3CK"
+echo "Telegram: L4NZ3CK"
+termux-setup-storage
+apt update
+apt upgrade
+apt install git
+apt install python
+apt install pip
+clear
+
